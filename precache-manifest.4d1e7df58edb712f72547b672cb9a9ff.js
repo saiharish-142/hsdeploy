f6e31b269e844c66ed1c4ccfd8e5e82857952201
Priority-Pulse-org <prioritypulse@gmail.com>
2020-12-01 23:00:41 +0530

    first commit

---
+++ precache-manifest.4d1e7df58edb712f72547b672cb9a9ff.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6febae9fcd99a450373897130d7b746f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f4651e8f0fa33ff2445",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "d10937a286c282770efc",
+    "url": "/static/css/main.be9028dd.chunk.css"
+  },
+  {
+    "revision": "0f4651e8f0fa33ff2445",
+    "url": "/static/js/2.c8cdc25e.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.c8cdc25e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d10937a286c282770efc",
+    "url": "/static/js/main.062b123b.chunk.js"
+  },
+  {
+    "revision": "f9c01ff14b74349c1174",
+    "url": "/static/js/runtime-main.9e0da1f5.js"
+  },
+  {
+    "revision": "3d9a511a8d12d62198331f33f363fe58",
+    "url": "/static/media/Profile.3d9a511a.png"
+  },
+  {
+    "revision": "bb0f91aa069c89a0370a789bf933de83",
+    "url": "/static/media/ambulancessvg.bb0f91aa.svg"
+  },
+  {
+    "revision": "ca5aa3247c520cfc6f22fd6918d264d7",
+    "url": "/static/media/facebook-brands.ca5aa324.svg"
+  },
+  {
+    "revision": "90a80ae2ddbe9b825dd3b0ba08f1519a",
+    "url": "/static/media/ham-dark.90a80ae2.svg"
+  },
+  {
+    "revision": "dacb148235a4193bbc05e77f201d9272",
+    "url": "/static/media/ham-light.dacb1482.svg"
+  },
+  {
+    "revision": "433034ac626ece7024126810d99d3f1d",
+    "url": "/static/media/hospitalmarkersvg.433034ac.svg"
+  },
+  {
+    "revision": "8db1adf3757ba687410165c97a3845f3",
+    "url": "/static/media/hospitalpic.8db1adf3.jpg"
+  },
+  {
+    "revision": "6173135a4b89e02486c33df71779c712",
+    "url": "/static/media/instagram-square-brands.6173135a.svg"
+  },
+  {
+    "revision": "6211187c2f9aca4212d1df99cb348551",
+    "url": "/static/media/linkedin-brands.6211187c.svg"
+  },
+  {
+    "revision": "db3d8ae2e32dc57458342a51da7e1cbc",
+    "url": "/static/media/twitter-square-brands.db3d8ae2.svg"
+  },
+  {
+    "revision": "ec707128b5f75f6579fc00adfcba773f",
+    "url": "/static/media/usermarkersvg.ec707128.svg"
+  }
+]);
